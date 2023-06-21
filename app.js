c618d89efb0b320c297bbcc1e003c1ac4356c7ca
MANI4C69 <136868864+MANI4C69@users.noreply.github.com>
2023-06-21 11:10:20 -0500

    h

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+// Configurar PUG como el motor de plantillas
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Ruta de inicio
+app.get('/', async (req, res) => {
+  try {
+    // Realizar solicitud a la API-FOOTBALL para obtener los datos de los equipos
+    const response = await axios.get('https://api-football.com/endpoint', {
+      headers: {
+        'X-RapidAPI-Key': 'TU_LLAVE_DE_AUTENTICACION',
+      },
+    });
+
+    // Pasar los datos obtenidos a la vista
+    res.render('index', { teams: response.data });
+  } catch (error) {
+    console.error(error);
+    res.render('error', { error: 'Ocurrió un error al obtener los datos de los equipos.' });
+  }
+});
+
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log('La plataforma está funcionando en el puerto 3000.');
+});
