3670cba934195566b4762f8ff7583f3d8e8a6f13
MANI4C69 <136868864+MANI4C69@users.noreply.github.com>
2023-06-20 21:45:45 -0500

    INICIO1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //PROYECTO ACM
+const express = require('éxpress');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.listen(3000, () =>{
+    console.log('server is runing')
+})
